388512fc59a6d9d85835fc04c96978541c246f32
vin <pennyyi97@gmail.com>
2023-05-09 15:52:46 +0900

    ENH: Query, Body decorator

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -8,24 +8,34 @@ export class MoviesController {
         return 'This will return all movies';
     }
 
-    @Get('/:id')
+    @Get('search') //:id 밑에 있으면 nest가 search를 id값으로 인식
+    search(@Query('year') searchingYear){
+        return`We are searching for a movie made after: ${searchingYear}`;
+    }
+
+    @Get(':id')
     getOnt(@Param("id")id){
         return `This will return one movie with the id: ${id}`;
     }
 
     @Post()
-    create(){
-        return 'This will create a movie';
+    create(@Body() movieData){
+        return movieData;
     }
 
-    @Delete('/:id')
+    @Delete(':id')
     remove(@Param('id') movieId){
         return `This will delete a movie with the id: ${movieId}`;
     }
 
-    @Patch('/:id') //리소스의 일부분만 업데이트
-    path(@Param('id')movieId){
-        return `This will patch a movie with the id: ${movieId}`;
+    @Patch(':id') //리소스의 일부분만 업데이트
+    path(@Param('id')movieId, @Body() updateData){
+        return {
+            updateedMovie: movieId,
+            ...updateData,
+        };
     }
+
+    
 }
 
