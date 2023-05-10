982da0c6e0fb8d2d1ab85f20006a521f1ec5342e
vin <pennyyi97@gmail.com>
2023-05-10 17:06:53 +0900

    TST: get 202,404 e2e 테스트

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes( new ValidationPipe({ //main에서 추가해준 pipe 를 test에도 동일하게 넣어주어야 올바른 테스트 진행 가능
+      whitelist:true,
+      forbidNonWhitelisted: true,
+      transform: true, 
+    }));
     await app.init();
   });
 
@@ -27,7 +32,7 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer())
         .get('/movies')
         .expect(200)
-        .expect([]); 
+        .expect([]);
         //expected [ { id: 1 } ] response body, got [] 
         //[]로 받았는데 [ { id: 1 } ]랑 같냐고 비교해서 에러 발생
     });
@@ -38,7 +43,7 @@ describe('AppController (e2e)', () => {
         .send({
           title:'Test',
           year: 2000,
-          geners: ['test']
+          genres: ['test']
         })
         .expect(201);
     });
@@ -50,4 +55,18 @@ describe('AppController (e2e)', () => {
     })
   });
 
+  describe('/movies/:id', () =>{
+    it("GET 200", () =>{
+      return request(app.getHttpServer())
+        .get('/movies/1')
+        .expect(200)
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer())
+        .get('/movies/999')
+        .expect(404)
+    })
+    it.todo("DELETE");
+    it.todo("PATCH");
+  })
 });
