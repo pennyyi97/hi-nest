209e698b6505080a01e969697a23adaa9239dc88
vin <pennyyi97@gmail.com>
2023-05-10 15:56:46 +0900

    TST: delete, create 유닛 테스트

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -46,7 +46,45 @@ describe('MoviesService', () => {
         expect(e).toBeInstanceOf(NotFoundException);
         expect(e.message).toEqual('Movie with Id 999 not found.')
       }
-    })
+    });
+  });
 
+  describe('deleteOne', () =>{
+    it('deletes a movie', () => {
+      service.create({
+        title: "Test Movie",
+        geners: ['test'],
+        year: 2000
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+
+    it('should return a 404', () =>{
+      try{
+        service.deleteOne(999);
+      } catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () =>{
+      const beforeCreate = service.getAll().length;
+
+      service.create({
+        title: "Test Movie",
+        geners: ['test'],
+        year: 2000
+      });
+
+      const afterCreate = service.getAll().length;
+      console.log(beforeCreate, afterCreate);
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
   })
 });
